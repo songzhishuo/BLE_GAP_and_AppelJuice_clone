939341fa184896686918151aac32001b0a29492a
songzhishuo <815794369@qq.com>
2024-02-05 16:44:00 +0800

    Add: 添加功能代码

--- src/main.c
+++ src/main.c
@@ -1,11 +1,13 @@
-/* main.c - Application main entry point */
-
-/*
- * Copyright (c) 2015-2016 Intel Corporation
- *
- * SPDX-License-Identifier: Apache-2.0
+/**
+ * @file main.c
+ * @author Argon
+ * @brief  BLE Apple 
+ * @version 0.1
+ * @date 2024-01-20
+ * 
+ * @copyright Copyright (c) 2024
+ * 
  */
-
 #include <zephyr/types.h>
 #include <stddef.h>
 #include <zephyr/sys/printk.h>
@@ -14,50 +16,83 @@
 #include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/bluetooth/hci.h>
 
-static uint8_t mfg_data[] = { 0xff, 0xff, 0x00 };
 
+//数据段
 static const struct bt_data ad[] = {
-	BT_DATA(BT_DATA_MANUFACTURER_DATA, mfg_data, 3),
+	BT_DATA_BYTES(BT_DATA_MANUFACTURER_DATA,
+				  0x4c, 0x00, 0x07, 0x19, 0x07, 0x0a, 0x20,
+				  0x75, 0xaa, 0x30, 0x01, 0x00, 0x00, 0x45,
+				  0x12, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00,
+				  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				  0x00)};
+
+//扫描响应数据段
+static const struct bt_data sd[] = {
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+	BT_DATA_BYTES(BT_DATA_PERIPHERAL_INT_RANGE, 0x20, 0x00, 0x40, 0x00),
+	BT_DATA_BYTES(BT_DATA_TX_POWER, 0x09),
 };
+
+//设备名
+#define DEVICE_NAME CONFIG_BT_DEVICE_NAME
+#define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
+
 
 int main(void)
 {
 	int err;
 
-	printk("Starting Broadcaster\n");
+	printk("BLE Starting\n");
 
 	/* Initialize the Bluetooth Subsystem */
 	err = bt_enable(NULL);
-	if (err) {
-		printk("Bluetooth init failed (err %d)\n", err);
+	if (err)
+	{
+		printk("Ble init failed (err %d)\n", err);
 		return 0;
 	}
 
-	printk("Bluetooth initialized\n");
+	printk("Ble initialized\n");
 
-	do {
-		k_msleep(1000);
-
-		printk("Sending advertising data: 0x%02X\n", mfg_data[2]);
-
-		/* Start advertising */
+	do
+	{
+		//开启广播
+		printk("Ble adv start\r\n");
 		err = bt_le_adv_start(BT_LE_ADV_NCONN, ad, ARRAY_SIZE(ad),
-				      NULL, 0);
-		if (err) {
+							  sd, ARRAY_SIZE(sd));
+		if (err)
+		{
 			printk("Advertising failed to start (err %d)\n", err);
 			return 0;
 		}
+		k_sleep(K_SECONDS(4));
 
-		k_msleep(1000);
-
+		//广播广播
+		printk("Ble adv stop\r\n");
 		err = bt_le_adv_stop();
-		if (err) {
+		if (err)
+		{
 			printk("Advertising failed to stop (err %d)\n", err);
 			return 0;
 		}
 
-		mfg_data[2]++;
+		//更新MAC
+		bt_addr_le_t local_mac_addr;
+		static bt_addr_le_t myaddr = {
+			.type = BT_ADDR_LE_RANDOM,
+			/* fixed MAC addres */
+			.a = {
+				.val = {0x01, 0x02, 0x03, 0x04, 0x05, 0xc6},
+			},
+		};
 
+		printk("Ble update MAC address\r\n");
+		err = bt_id_create(&myaddr, NULL);
+		if (err < 0)
+		{
+			printk("bt_id_create err!\r\n");
+		}
+		k_sleep(K_SECONDS(4));
 	} while (1);
 	return 0;
 }
